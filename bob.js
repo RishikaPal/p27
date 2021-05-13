db82a9d87abdb313c4b85ecc7a716961c1c8b863
RishikaPal <80198528+RishikaPal@users.noreply.github.com>
2021-05-13 20:29:46 +0530

    Add files via upload

--- bob.js
+++ bob.js
@@ -16,6 +16,20 @@ class bob {
           this.body=Bodies.circle(this.x, this.y, this.r/2, options) 
 
           World.add(world, this.body);
+
+          bobDiameter=40; startBobPositionX=width/2;
+          
+           startBobPositionY=height/4+500; 
+           
+           bobObj1=new bob(startBobPositionX-bobDiameter*2,startBobPositionY,bobDiameter);
+           
+            bobObj2=new bob(startBobPositionX-bobDiameter,startBobPositionY,bobDiameter);
+            
+             bobObj3=new bob(startBobPositionX,startBobPositionY,bobDiameter);
+             
+              bobObj4=new bob(startBobPositionX+bobDiameter,startBobPositionY,bobDiameter);
+          
+           bobObj5=new bob(startBobPositionX+bobDiameter*2,startBobPositionY,bobDiameter);
     }
              display() { 
 
